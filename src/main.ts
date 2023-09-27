ae1011e331a9472a005d6dc49064ed9199f5d6ed
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-27 16:47:33 +0700

    Pipe,Validation,DTO - Data Transfer Object

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(configService.get<string>('PORT'));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEAN, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Email must be an email' })
+  @IsNotEmpty({ message: 'Please Enter Your Email' })
   email: string;
+  @IsNotEmpty({ message: 'Please Enter Your Password' })
   password: string;
   name: string;
   address: string;
