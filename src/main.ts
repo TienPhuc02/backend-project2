c8d601165e9c9bde67d037223288c21205e91769
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-27 21:23:01 +0700

    Setup Cors NestJS

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,17 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  
+
   //global jwt guard
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    "origin": '*',
+    "methods": 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+  });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(configService.get<string>('PORT'));
 }
