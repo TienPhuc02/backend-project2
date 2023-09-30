621a02647041a1e27cefef1f5c77f89c6fe1ce6c
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-30 10:07:18 +0700

    Update User Schema,Dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,60 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
+import mongoose from 'mongoose';
 
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  name: string;
+}
 export class CreateUserDto {
-  @IsEmail({}, { message: 'Email must be an email' })
+  @IsNotEmpty({ message: 'Please Enter Your Name' })
+  name: string;
   @IsNotEmpty({ message: 'Please Enter Your Email' })
+  @IsEmail({}, { message: 'Email Must Be An Email' })
   email: string;
   @IsNotEmpty({ message: 'Please Enter Your Password' })
   password: string;
-  name: string;
-  address: string;
+  @IsNotEmpty({ message: 'Please Enter Your Age' })
   age: number;
+  @IsNotEmpty({ message: 'Please Enter Your Gender' })
+  gender: string;
+  @IsNotEmpty({ message: 'Please Enter Your Address' })
+  address: string;
+  @IsNotEmpty({ message: 'Please Enter Your Role' })
+  role: string;
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Please Enter Your Name',
+  })
+  name: string;
+  @IsEmail({}, { message: 'Invalid email message' })
+  @IsNotEmpty({
+    message: 'Please Enter Your Email',
+  })
+  email: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Password',
+  })
+  password: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Age',
+  })
+  age: number;
+  @IsNotEmpty({
+    message: 'Please Enter Your Gender',
+  })
+  gender: string;
+  @IsNotEmpty({
+    message: 'Please Enter Your Address',
+  })
+  address: string;
 }
