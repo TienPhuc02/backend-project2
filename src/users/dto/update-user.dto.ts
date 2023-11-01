5199d8442c79cf2763a020cbd8b452efbb407f94
TienPhuc02 <dtp2352002@gmail.com>
2023-11-01 19:28:23 +0700

    update user schema

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,39 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import mongoose from 'mongoose';
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
   name: string;
-  address: string;
+}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsNotEmpty({ message: 'Please Enter Your Name' })
+  name: string;
+  @IsNotEmpty({ message: 'Please Enter Your Email' })
+  @IsEmail({}, { message: 'Email Must Be An Email' })
+  email: string;
+  @IsNotEmpty({ message: 'Please Enter Your Password' })
+  password: string;
+  @IsNotEmpty({ message: 'Please Enter Your Age' })
   age: number;
+  @IsNotEmpty({ message: 'Please Enter Your Gender' })
+  gender: string;
+  @IsNotEmpty({ message: 'Please Enter Your Address' })
+  address: string;
+  @IsNotEmpty({ message: 'Please Enter Your Role' })
+  role: string;
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
 }
