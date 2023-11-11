83c207828fe8ae45274246335a156a0fb965fc86
TienPhuc02 <dtp2352002@gmail.com>
2023-11-11 16:23:40 +0700

    CRUD permissions

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -1,34 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
-  create(@Body() createPermissionDto: CreatePermissionDto) {
-    return this.permissionsService.create(createPermissionDto);
+  @ResponseMessage('Create A New Permission Success!!')
+  create(
+    @Body() createPermissionDto: CreatePermissionDto,
+    @User() user: IUser,
+  ) {
+    return this.permissionsService.create(createPermissionDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.permissionsService.findAll();
+  @ResponseMessage('Create A New Permission Success!!')
+  findAll(
+    @Query('current') current: number,
+    @Query('pageSize') pageSize: number,
+    @Query() qs: string,
+  ) {
+    return this.permissionsService.findAll(current, pageSize, qs);
   }
 
   @Get(':id')
+  @ResponseMessage('Get A New Permission Success!!')
   findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+    return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  @ResponseMessage('Update A New Permission Success!!')
+  update(
+    @Param('id') id: string,
+    @Body() updatePermissionDto: UpdatePermissionDto,
+    @User() user: IUser,
+  ) {
+    return this.permissionsService.update(id, updatePermissionDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  @ResponseMessage('Create A New Permission Success!!')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.permissionsService.remove(id, user);
   }
 }
--- src/permissions/permissions.service.ts
+++ src/permissions/permissions.service.ts
@@ -1,26 +1,133 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Permission, PermissionDocument } from './schema/permission.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import aqp from 'api-query-params';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class PermissionsService {
-  create(createPermissionDto: CreatePermissionDto) {
-    return 'This action adds a new permission';
+  constructor(
+    @InjectModel(Permission.name)
+    private permissionModel: SoftDeleteModel<PermissionDocument>,
+  ) {}
+
+  create = async (createPermissionDto: CreatePermissionDto, user: IUser) => {
+    const { apiPath, method, name, module } = createPermissionDto;
+    const existingPermisson = await this.permissionModel.findOne({
+      apiPath,
+      method,
+    });
+    if (existingPermisson) {
+      throw new BadRequestException(
+        `API ${apiPath} với method ${method} đã tồn tại vui lòng thêm API với method khác.`,
+      );
+    }
+    const newPermission = await this.permissionModel.create({
+      apiPath,
+      method,
+      name,
+      module,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+    return {
+      _id: newPermission._id,
+      createdAt: newPermission.createdAt,
+    };
+  };
+
+  async findAll(current: number, pageSize: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+    const offset: number = (+current - 1) * +pageSize;
+    const defaultLimit = +pageSize ? +pageSize : 10;
+    const totalItems = (await this.permissionModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    if ((sort as any) === '-updatedAt') {
+      // @ts-ignore: Unreachable code error
+      sort = '-updatedAt';
+    }
+    if ((sort as any) === '-name') {
+      // @ts-ignore: Unreachable code error
+      sort = '-name';
+    }
+    if ((sort as any) === '-apiPath') {
+      // @ts-ignore: Unreachable code error
+      sort = '-apiPath';
+    }
+    if ((sort as any) === '-method') {
+      // @ts-ignore: Unreachable code error
+      sort = '-method';
+    }
+    if ((sort as any) === '-createdAt') {
+      // @ts-ignore: Unreachable code error
+      sort = '-createdAt';
+    }
+    const result = await this.permissionModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .select('-password')
+      .exec();
+    return {
+      meta: {
+        current: current, //trang hien tai
+        pageSize: pageSize, // so luong ban ghi 1 trang
+        pages: totalPages, //tong so trang
+        total: totalItems, //tong so ban ghi
+      },
+      result,
+    };
   }
 
-  findAll() {
-    return `This action returns all permissions`;
+  async findOne(id: string) {
+    return await this.permissionModel.findOne({ _id: id });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permission`;
-  }
+  update = async (
+    id: string,
+    updatePermissionDto: UpdatePermissionDto,
+    user: IUser,
+  ) => {
+    const { apiPath, method, name, module } = updatePermissionDto;
+    const newPermissionUpdate = await this.permissionModel.updateOne(
+      { _id: id },
+      {
+        apiPath,
+        method,
+        name,
+        module,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return newPermissionUpdate;
+  };
 
-  update(id: number, updatePermissionDto: UpdatePermissionDto) {
-    return `This action updates a #${id} permission`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} permission`;
-  }
+  remove = async (id: string, user: IUser) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'not found permission';
+    }
+    await this.permissionModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.permissionModel.softDelete({ _id: id });
+  };
 }
