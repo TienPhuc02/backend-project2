e7820f4afcfb2ab4614d45ef9e5fe2ba814399c5
TienPhuc02 <dtp2352002@gmail.com>
2023-11-11 16:34:27 +0700

    CRUD role module

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
+  @ResponseMessage('Get A New Role Success!!')
+  create(@Body() createRoleDto: CreateRoleDto, @User() user: IUser) {
+    return this.rolesService.create(createRoleDto, user);
   }
-
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  @ResponseMessage('Create A New Permission Success!!')
+  findAll(
+    @Query('current') current: number,
+    @Query('pageSize') pageSize: number,
+    @Query() qs: string,
+  ) {
+    return this.rolesService.findAll(current, pageSize, qs);
   }
 
   @Get(':id')
+  @ResponseMessage('Get A New Permission Success!!')
   findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+    return this.rolesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  @ResponseMessage('Update A New Permission Success!!')
+  update(
+    @Param('id') id: string,
+    @Body() updateRoleDto: UpdateRoleDto,
+    @User() user: IUser,
+  ) {
+    return this.rolesService.update(id, updateRoleDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  @ResponseMessage('Create A New Permission Success!!')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.rolesService.remove(id, user);
   }
 }
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,26 +1,115 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schema/role.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectModel(Role.name)
+    private roleModel: SoftDeleteModel<RoleDocument>,
+  ) {}
+  async create(createRoleDto: CreateRoleDto, user: IUser) {
+    const { description, isActive, name, permissions } = createRoleDto;
+    const newRole = await this.roleModel.create({
+      description,
+      isActive,
+      name,
+      permissions,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+    return {
+      _id: newRole._id,
+      createdAt: newRole.createdAt,
+    };
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(current: number, pageSize: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+    const offset: number = (+current - 1) * +pageSize;
+    const defaultLimit = +pageSize ? +pageSize : 10;
+    const totalItems = (await this.roleModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+    if ((sort as any) === '-updatedAt') {
+      // @ts-ignore: Unreachable code error
+      sort = '-updatedAt';
+    }
+    if ((sort as any) === '-description') {
+      // @ts-ignore: Unreachable code error
+      sort = '-description';
+    }
+    if ((sort as any) === '-createdAt') {
+      // @ts-ignore: Unreachable code error
+      sort = '-createdAt';
+    }
+    const result = await this.roleModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .select('-password')
+      .exec();
+    return {
+      meta: {
+        current: current, //trang hien tai
+        pageSize: pageSize, // so luong ban ghi 1 trang
+        pages: totalPages, //tong so trang
+        total: totalItems, //tong so ban ghi
+      },
+      result,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: string) {
+    return await this.roleModel.findOne({ _id: id });
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
+  update = async (id: string, updateRoleDto: UpdateRoleDto, user: IUser) => {
+    const { description, isActive, name, permissions } = updateRoleDto;
+    const newPermissionUpdate = await this.roleModel.updateOne(
+      { _id: id },
+      {
+        description,
+        isActive,
+        name,
+        permissions,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return newPermissionUpdate;
+  };
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
-  }
+  remove = async (id: string, user: IUser) => {
+    const foundRole = await this.roleModel.findById(id);
+    if(foundRole.name==="ADMIN"){
+      throw new BadRequestException("không thể xóa role ADMIN")
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return 'not found permission';
+    }
+    await this.roleModel.updateOne(
+      { _id: id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.roleModel.softDelete({ _id: id });
+  };
 }
