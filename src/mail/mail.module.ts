1c1cbe6643a7a21fb18acfa939492dbc513731da
TienPhuc02 <dtp2352002@gmail.com>
2023-11-16 14:54:01 +0700

    nestjs-mailer

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,34 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
+import { ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          type: 'SMTP',
+          host: configService.get<string>('EMAIL_HOST'),
+          secure: true,
+          port: 465,
+          auth: {
+            user: configService.get<string>('EMAIL_AUTH_USER'),
+            pass: configService.get<string>('EMAIL_AUTH_PASS'),
+          },
+        },
+        // template: {
+        // dir: join(__dirname, 'templates'),
+        // adapter: new HandlebarsAdapter(),
+        // options: {
+        // strict: true,
+        // },
+        // },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MailController],
   providers: [MailService],
 })
